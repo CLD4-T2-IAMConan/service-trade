5bacfcd987207366589b70841dc53bf3e3cc9555
yyyyyyei <ye0126jin@naver.com>
2025-12-08 17:23:35 +0900

    chore : 버전이 없는 파일 push

---
+++ src/test/java/com/company/trade/TradeApplicationTests.java
@@ -0,0 +1,12 @@
+package com.company.trade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
