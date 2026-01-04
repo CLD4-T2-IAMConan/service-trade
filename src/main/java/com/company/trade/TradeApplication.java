5218dad511d3d5c3136a00f6ab15088ed0c3940b
motionbit95 <motionbit95@example.com>
2026-01-04 19:19:25 +0900

    refactor: Remove SnsConfig import and streamline component scanning in TradeApplication

--- src/main/java/com/company/trade/TradeApplication.java
+++ src/main/java/com/company/trade/TradeApplication.java
@@ -1,16 +1,13 @@
 package com.company.trade;
 
-import com.company.sns.config.SnsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.company.trade", "com.company.sns"})
-@Import(SnsConfig.class)
 public class TradeApplication {
 
     public static void main(String[] args) {
